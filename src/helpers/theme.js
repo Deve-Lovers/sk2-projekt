c9d825eb28e5bf7358477eb3b52e1094ab3d0dac
Aleksandra Kozak <a.kozak@expansio.pl>
2023-09-03 17:40:22 +0200

    Add base color palette

---
+++ src/helpers/theme.js
@@ -0,0 +1,27 @@
+export const theme = {
+  colors: {
+    actionPrimary: 'rgba(0,150,209,1)',
+    textOnPrimary: 'rgba(242,244,248,1)',
+    borderOnPrimary: 'rgba(0,150,209,1)',
+
+    actionPrimaryReversed: 'rgba(242,244,248,1)',
+    textOnPrimaryReversed: 'rgba(0,150,209,1)',
+    borderOnPrimaryReversed: 'rgba(0,150,209,1)',
+
+    actionSecondary: 'rgba(245,57,102,1)',
+    textOnSecondary: 'rgba(255,244,234,1)',
+    borderOnSecondary: 'rgba(245,57,102,1)',
+
+    actionSecondaryReversed: 'rgba(255,244,234,1)',
+    textOnSecondaryReversed: 'rgba(245,57,102,1)',
+    borderOnSecondaryReversed: 'rgba(245,57,102,1)',
+
+    actionDisabled: 'transparent',
+    textOnDisabled: 'transparent',
+    borderOnDisabled: 'transparent',
+
+    background: 'rgba(169,226,245,1)',
+    surface: 'rgba(242,244,248,1)',
+    shadow: 'rgba(0,0,0,1)',
+  },
+};
