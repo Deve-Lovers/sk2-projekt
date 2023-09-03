44f08305911b0277aeb3e6d5c67be4c3bf15a834
Aleksandra Kozak <a.kozak@expansio.pl>
2023-09-04 00:26:01 +0200

    Add SafeAreaView in the app

--- App.tsx
+++ App.tsx
@@ -1,8 +1,15 @@
 import * as React from 'react';
+import { SafeAreaView } from 'react-native';
+
 import AppStack from 'petCare/src/navigation';
+import { theme } from 'petCare/src/helpers/theme';
 
 function App(): JSX.Element {
-  return <AppStack />;
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: theme.colors.background }}>
+      <AppStack />
+    </SafeAreaView>
+  );
 }
 
 export default App;
