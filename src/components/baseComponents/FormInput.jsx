517a8862e4e9467c8eaa9570b8bcb94a688647e8
Aleksandra Kozak <a.kozak@expansio.pl>
2023-09-03 18:47:46 +0200

    Add simple view of FormInput

---
+++ src/components/baseComponents/FormInput.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, TouchableOpacity, TextInput } from 'react-native';
+import PropTypes from 'prop-types';
+import { theme } from 'petCare/src/helpers/theme';
+
+function FormInput({ width, height }) {
+  return (
+    <TouchableOpacity style={styles.container(width, height)}>
+      <TextInput />
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: (width, height) => ({
+    backgroundColor: theme.colors.surface,
+    shadowOffset: { width: 0, height: 2 },
+    shadowColor: theme.colors.shadow,
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    elevation: 5,
+    marginVertical: 10,
+    borderRadius: 30,
+    width,
+    height,
+  }),
+});
+
+FormInput.propTypes = {
+  width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
+
+FormInput.defaultProps = {
+  width: '100%',
+  height: 50,
+};
+
+export default FormInput;
