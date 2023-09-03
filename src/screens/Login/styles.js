3cbe96def2d13f732a2b51069dc7c04a35b36b1f
Aleksandra Kozak <a.kozak@expansio.pl>
2023-09-03 19:11:34 +0200

    Add basic styling for Login screens

---
+++ src/screens/Login/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+const logotype = {
+  resizeMode: 'contain',
+  marginBottom: 40,
+  marginTop: 100,
+  width: '100%',
+  height: 200,
+};
+
+export const styles = StyleSheet.create({
+  logotype,
+});
+
+// <Button onPress={() => navigation.navigate('Main')} title="Go to Home" />
