069a2740c5374c06243c2a0b324612598838faab
Aleksandra Kozak <a.kozak@expansio.pl>
2023-09-03 17:45:21 +0200

    Modify Login navigation to use new register screens

--- src/navigation/LoginStack.tsx
+++ src/navigation/LoginStack.tsx
@@ -1,36 +1,20 @@
 import * as React from 'react';
-import { Button, Text, View } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function LoginScreen1({ navigation }): JSX.Element {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Login Screen 1</Text>
-      <Button onPress={() => navigation.navigate('Login2')} title="Go to second Login screen" />
-    </View>
-  );
-}
+import SignIn from 'petCare/src/screens/Login/SignIn';
+import SignUp from 'petCare/src/screens/Login/SignUp';
 
-function LoginScreen2({ navigation }): JSX.Element {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Login Screen 2</Text>
-      <Button onPress={() => navigation.navigate('Main')} title="Go to Home" />
-    </View>
-  );
-}
-
-const Tab = createNativeStackNavigator();
+const Login = createNativeStackNavigator();
 
 function LoginStack(): JSX.Element {
   return (
-    <Tab.Navigator
-      initialRouteName="Login1"
+    <Login.Navigator
+      initialRouteName="SignIn"
       screenOptions={{ headerShown: false, gestureEnabled: false }}
     >
-      <Tab.Screen name="Login1" component={LoginScreen1} />
-      <Tab.Screen name="Login2" component={LoginScreen2} />
-    </Tab.Navigator>
+      <Login.Screen name="SignIn" component={SignIn} />
+      <Login.Screen name="SignUp" component={SignUp} />
+    </Login.Navigator>
   );
 }
 
