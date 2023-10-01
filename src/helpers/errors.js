b15518a11c5cbc09ead45246479329128959b1a6
Aleksandra Kozak <a.kozak@expansio.pl>
2023-10-01 13:12:44 +0200

    Update errors base

--- src/helpers/errors.js
+++ src/helpers/errors.js
@@ -1,11 +1,15 @@
 export const errorMessage = (error) => {
   switch (error) {
     case 'Invalid user!':
-      return 'Błąd logowania - sprawdź login oraz hasło';
+      return 'Błąd logowania - sprawdź login';
+    case 'Invalid password!':
+      return 'Błąd logowania - błędne hasło';
     case 'Missing fields':
       return 'Wypełnij brakujące pola';
     case 'Invalid email':
       return 'Błędny format adresu email';
+    case 'Account already exists':
+      return 'Konto o podanym adresu email już istnieje';
     default:
       return 'Nieznany błąd';
   }
