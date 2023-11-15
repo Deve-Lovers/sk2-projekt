5b7ac876efc5d24ef727262cf9c9fcd8f692afb2
Aleksandra Kozak <a.kozak@expansio.pl>
2023-11-15 15:29:43 +0100

    Add closing button visibility prop

--- client/src/components/baseComponents/ScreenHeader.jsx
+++ client/src/components/baseComponents/ScreenHeader.jsx
@@ -6,7 +6,7 @@ import Close from 'sk/src/assets/icons/close.png';
 
 import { theme } from 'sk/src/helpers/theme';
 
-function ScreenHeader({ title, navigation }) {
+function ScreenHeader({ title, isButtonVisible, navigation }) {
   const handleClose = () => {
     navigation.goBack();
   };
@@ -14,9 +14,11 @@ function ScreenHeader({ title, navigation }) {
   return (
     <View style={styles.header}>
       <Text style={styles.text}>{title}</Text>
-      <TouchableOpacity onPress={handleClose}>
-        <Image source={Close} style={styles.icon} />
-      </TouchableOpacity>
+      {isButtonVisible && (
+        <TouchableOpacity onPress={handleClose}>
+          <Image source={Close} style={styles.icon} />
+        </TouchableOpacity>
+      )}
     </View>
   );
 }
@@ -46,6 +48,11 @@ const styles = StyleSheet.create({
 
 ScreenHeader.propTypes = {
   title: PropTypes.string.isRequired,
+  isButtonVisible: PropTypes.bool,
+};
+
+ScreenHeader.defaultProps = {
+  isButtonVisible: false,
 };
 
 export default ScreenHeader;
