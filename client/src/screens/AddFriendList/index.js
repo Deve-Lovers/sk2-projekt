6248b126aa8037b74750bc1f9624ce5b1ec6b1a3
Aleksandra Kozak <a.kozak@expansio.pl>
2023-12-28 20:11:36 +0100

    Modify AddFriends screen to display data from database

--- client/src/screens/AddFriendList/index.js
+++ client/src/screens/AddFriendList/index.js
@@ -1,13 +1,12 @@
 import React, { useEffect } from 'react';
 import ListItem from 'sk/src/components/baseComponents/ListItem';
 import Screen from 'sk/src/components/baseComponents/Screen';
-import { users } from 'sk/src/helpers/mocks/usersMock';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import { theme } from 'sk/src/helpers/theme';
 import { connect } from 'react-redux';
 import { getOtherUsersList } from 'sk/store/Friends/actions';
 
-function AddFriendList({ getOtherUsersList: _getOtherUsersList }) {
+function AddFriendList({ getOtherUsersList: _getOtherUsersList, otherFriends, isPending, error }) {
   const message = 'Brak nowych osób do dodania. \nSprawdź zakładkę "Znajomi"';
 
   useEffect(() => {
@@ -20,17 +19,30 @@ function AddFriendList({ getOtherUsersList: _getOtherUsersList }) {
     </View>
   );
 
-  return (
-    <Screen>
-      {users.map((user, index) => (
-        <ListItem name={`${user.name} ${user.surname}`} addFriend color={index % 5} />
-      ))}
-      {!users && renderNoContacts()}
-    </Screen>
-  );
+  const renderContent = () => {
+    if (error) {
+      return renderNoContacts();
+    }
+
+    if (isPending) {
+      return <Text>Loading...</Text>;
+    }
+
+    return (
+      <FlatList
+        data={otherFriends}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item, index }) => (
+          <ListItem name={`${item.name} ${item.surname}`} addFriend color={index % 5} />
+        )}
+      />
+    );
+  };
+
+  return <Screen>{renderContent()}</Screen>;
 }
 const mapStateToProps = (state) => ({
-  userFriends: state.friends.userFriends,
+  otherFriends: state.friends.otherFriends,
   isPending: state.friends.isPending,
   error: state.friends.error,
 });
