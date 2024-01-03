8baf2f08f0303d252d919059c7a8367783167cb3
Aleksandra Kozak <a.kozak@expansio.pl>
2024-01-03 12:53:37 +0100

    Adjust Profile screen

--- client/src/screens/Settings.js
+++ client/src/screens/Settings.js
@@ -1,16 +1,31 @@
-import { Button, Text, View } from 'react-native';
-import { userLogout } from 'sk/store/Auth/actions';
+import React from 'react';
 import { connect } from 'react-redux';
-import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import { userLogout } from 'sk/store/Auth/actions';
+import Screen from 'sk/src/components/baseComponents/Screen';
+import Button from 'sk/src/components/baseComponents/Button';
+import { theme } from 'sk/src/helpers/theme';
 
 function Settings(props) {
-  const { navigation } = props;
+  const welcomeText = 'Witaj w aplikacji WebTap!\n';
+  const addFriendsText =
+    '- W zakładce "Dodaj znajomych" możesz dodać innych użytkowników do swojej listy znajomych\n\n';
+  const friendsText = '- W zakładce "Znajomi" możesz czatować z poszczególnymi znajomymi\n\n';
+  const logoutText = '- Jeśli chcesz się wylogować, kliknij przycisk poniżej ⬇️';
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Settings Screen</Text>
-      <Button onPress={() => navigation.navigate('Home')} title="Go to Home" />
-      <Button onPress={() => props.userLogout()} title="Logout" />
-    </View>
+    <Screen>
+      <View style={styles.container}>
+        <Text style={styles.welcomeText}>{welcomeText}</Text>
+        <Text style={styles.text}>
+          {addFriendsText}
+          {friendsText}
+          {logoutText}
+        </Text>
+      </View>
+      <Button title="Wyloguj mnie" variant="primaryFocused" onPress={() => props.userLogout()} />
+    </Screen>
   );
 }
 
@@ -19,3 +34,29 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 export default connect(null, mapDispatchToProps)(Settings);
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: theme.colors.surface,
+    shadowOffset: { width: 0, height: 2 },
+    shadowColor: theme.colors.shadow,
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    elevation: 5,
+    marginVertical: 10,
+    padding: 20,
+    borderRadius: 30,
+    width: '100%',
+    height: 370,
+  },
+  welcomeText: {
+    color: theme.colors.darkText,
+    fontWeight: '700',
+    fontSize: 25,
+  },
+  text: {
+    color: theme.colors.darkText,
+    fontWeight: '500',
+    fontSize: 20,
+  },
+});
