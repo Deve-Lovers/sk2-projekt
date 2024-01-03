0c96cb79cc0f7bc22fcfc8cd2ac6ec2f42ca545b
Aleksandra Kozak <a.kozak@expansio.pl>
2024-01-03 13:25:53 +0100

    Change tab names

--- client/src/navigation/MainStack.tsx
+++ client/src/navigation/MainStack.tsx
@@ -3,18 +3,26 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import FriendsList from 'sk/src/screens/FriendsList';
 import AddFriendList from 'sk/src/screens/AddFriendList';
-import Settings from 'sk/src/screens/Profile';
+import Profile from 'sk/src/screens/Profile';
 import Chat from 'sk/src/screens/Chat';
+import { theme } from 'sk/src/helpers/theme';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 function TabStack(): JSX.Element {
   return (
-    <Tab.Navigator initialRouteName="FriendsList" screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="FriendsList" component={FriendsList} />
-      <Tab.Screen name="AddFriendList" component={AddFriendList} />
-      <Tab.Screen name="Profile" component={Settings} />
+    <Tab.Navigator
+      initialRouteName="FriendsList"
+      screenOptions={{ headerShown: false, tabBarActiveTintColor: theme.colors.actionPrimary }}
+    >
+      <Tab.Screen name="FriendsList" component={FriendsList} options={{ title: 'Znajomi' }} />
+      <Tab.Screen
+        name="AddFriendList"
+        component={AddFriendList}
+        options={{ title: 'Dodaj znajomych' }}
+      />
+      <Tab.Screen name="Profile" component={Profile} options={{ title: 'Twój profil' }} />
     </Tab.Navigator>
   );
 }
