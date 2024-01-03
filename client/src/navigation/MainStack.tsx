a19aa21c44436a4273ca2e60b8e77fd18eb4ffdd
Aleksandra Kozak <a.kozak@expansio.pl>
2024-01-03 13:51:59 +0100

    Change icons in bottom menu

--- client/src/navigation/MainStack.tsx
+++ client/src/navigation/MainStack.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import AddUser from 'sk/src/assets/icons/add-user.png';
+import Friends from 'sk/src/assets/icons/friends.png';
+import UserProfile from 'sk/src/assets/icons/user.png';
+
 import FriendsList from 'sk/src/screens/FriendsList';
 import AddFriendList from 'sk/src/screens/AddFriendList';
 import Profile from 'sk/src/screens/Profile';
 import Chat from 'sk/src/screens/Chat';
 import { theme } from 'sk/src/helpers/theme';
+import { Image, StyleSheet } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -16,13 +22,30 @@ function TabStack(): JSX.Element {
       initialRouteName="FriendsList"
       screenOptions={{ headerShown: false, tabBarActiveTintColor: theme.colors.actionPrimary }}
     >
-      <Tab.Screen name="FriendsList" component={FriendsList} options={{ title: 'Znajomi' }} />
+      <Tab.Screen
+        name="FriendsList"
+        component={FriendsList}
+        options={{
+          title: 'Znajomi',
+          tabBarIcon: () => <Image source={Friends} style={styles.icon} />,
+        }}
+      />
       <Tab.Screen
         name="AddFriendList"
         component={AddFriendList}
-        options={{ title: 'Dodaj znajomych' }}
+        options={{
+          title: 'Dodaj znajomych',
+          tabBarIcon: () => <Image source={AddUser} style={styles.icon} />,
+        }}
       />
-      <Tab.Screen name="Profile" component={Profile} options={{ title: 'Twój profil' }} />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          title: 'Twój profil',
+          tabBarIcon: () => <Image source={UserProfile} style={styles.icon} />,
+        }}
+      />
     </Tab.Navigator>
   );
 }
@@ -37,3 +60,12 @@ function MainStack(): JSX.Element {
 }
 
 export default MainStack;
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 25,
+    height: 25,
+    resizeMode: 'contain',
+    tintColor: theme.colors.actionPrimary,
+  },
+});
