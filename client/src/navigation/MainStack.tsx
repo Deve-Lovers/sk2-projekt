bb8280f4878bc6196b6afc793a3c14145ef77e8f
Aleksandra Kozak <a.kozak@expansio.pl>
2024-01-03 12:53:21 +0100

    Add Profile screen to menu

--- client/src/navigation/MainStack.tsx
+++ client/src/navigation/MainStack.tsx
@@ -3,6 +3,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import FriendsList from 'sk/src/screens/FriendsList';
 import AddFriendList from 'sk/src/screens/AddFriendList';
+import Settings from 'sk/src/screens/Settings';
 import Chat from 'sk/src/screens/Chat';
 
 const Tab = createBottomTabNavigator();
@@ -13,6 +14,7 @@ function TabStack(): JSX.Element {
     <Tab.Navigator initialRouteName="FriendsList" screenOptions={{ headerShown: false }}>
       <Tab.Screen name="FriendsList" component={FriendsList} />
       <Tab.Screen name="AddFriendList" component={AddFriendList} />
+      <Tab.Screen name="Profile" component={Settings} />
     </Tab.Navigator>
   );
 }
